1454dd118ff3ea63aba571204a9b0fc480a645f1
ignaciotb <ignaciotorroba@hotmail.com>
2021-08-10 11:47:36 +0200

    Function to easily save SLAM resultson txt

--- examples/VictoriaSLAMExample.cpp
+++ examples/VictoriaSLAMExample.cpp
@@ -9,59 +9,17 @@
 
  * -------------------------------------------------------------------------- */
 
-/**
- * @file PlanarSLAMExample.cpp
- * @brief Simple robotics example using odometry measurements and bearing-range (laser) measurements
- * @author Alex Cunningham
- */
-
-/**
- * A simple 2D planar slam example with landmarks
- *  - The robot and landmarks are on a 2 meter grid
- *  - Robot poses are facing along the X axis (horizontal, to the right in 2D)
- *  - The robot moves 2 meters each step
- *  - We have full odometry between poses
- *  - We have bearing and range information for measurements
- *  - Landmarks are 2 meters away from the robot trajectory
- */
-
-// As this is a planar SLAM example, we will use Pose2 variables (x, y, theta) to represent
-// the robot positions and Point2 variables (x, y) to represent the landmark coordinates.
 #include <gtsam/geometry/Pose2.h>
 #include <gtsam/geometry/Point2.h>
 
-// Each variable in the system (poses and landmarks) must be identified with a unique key.
-// We can either use simple integer keys (1, 2, 3, ...) or symbols (X1, X2, L1).
-// Here we will use Symbols
 #include <gtsam/inference/Symbol.h>
 
-// In GTSAM, measurement functions are represented as 'factors'. Several common factors
-// have been provided with the library for solving robotics/SLAM/Bundle Adjustment problems.
-// Here we will use a RangeBearing factor for the range-bearing measurements to identified
-// landmarks, and Between factors for the relative motion described by odometry measurements.
-// Also, we will initialize the robot at the origin using a Prior factor.
 #include <gtsam/slam/BetweenFactor.h>
 #include <gtsam/sam/BearingRangeFactor.h>
 
-// When the factors are created, we will add them to a Factor Graph. As the factors we are using
-// are nonlinear factors, we will need a Nonlinear Factor Graph.
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
-
-// Finally, once all of the factors have been added to our factor graph, we will want to
-// solve/optimize to graph to find the best (Maximum A Posteriori) set of variable values.
-// GTSAM includes several nonlinear optimizers to perform this step. Here we will use the
-// common Levenberg-Marquardt solver
 #include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
-
-// Once the optimized values have been calculated, we can also calculate the marginal covariance
-// of desired variables
 #include <gtsam/nonlinear/Marginals.h>
-
-// The nonlinear solvers within GTSAM are iterative solvers, meaning they linearize the
-// nonlinear functions around an initial linearization point, then solve the linear system
-// to update the linearization point. This happens repeatedly until the solver converges
-// to a consistent set of variable values. This requires us to specify an initial guess
-// for each variable, held in a Values container.
 #include <gtsam/nonlinear/Values.h>
 
 #include <gtsam/slam/dataset.h>
@@ -69,10 +27,44 @@
 using namespace std;
 using namespace gtsam;
 
+void saveResults(const Values& result, const string& outfilename)
+{
+    fstream stream(outfilename.c_str(), fstream::out);
+
+    auto index = [](gtsam::Key key){ return Symbol(key).index(); };
+
+    // save 2D poses
+    for (const auto key_value : result)
+    {
+        auto p = dynamic_cast<const GenericValue<Pose2> *>(&key_value.value);
+        if (!p)
+            continue;
+        const Pose2 &pose = p->value();
+        stream << "Pose " << index(key_value.key) << " " << pose.x() << " "
+               << pose.y() << " " << pose.theta() << endl;
+    }
+
+    // save 2D landmarks
+    for (const auto key_value : result)
+    {
+        auto p = dynamic_cast<const GenericValue<Point2> *>(&key_value.value);
+        if (!p)
+            continue;
+        const Point2 &point = p->value();
+        stream << "Landmark " << index(key_value.key) << " " << point.x() << " "
+               << point.y() << endl;
+    }
+
+    stream.close();
+    std::cout << "Results saved" << std::endl;
+}
+
 int main(int argc, char** argv) {
   
   // Load Victoria dataset
   const string filename = findExampleDataFile("victoria_park_test.txt");
+  const string outfilename = "victoria_test_out.txt";
+  
   // Create a factor graph
   NonlinearFactorGraph::shared_ptr graph;
   Values::shared_ptr initialEstimate;
@@ -84,17 +76,17 @@ int main(int argc, char** argv) {
 
   // Print
   initialEstimate->print("Initial Estimate:\n");
-  
-  // Save for testing
-//   std::cout << "Saving graph" << std::endl;
-//   save2D(*graph, *initialEstimate, nullptr, "victoria_test_out.txt");
-//   std::cout << "Graph saved" << std::endl;
 
   // Optimize
   LevenbergMarquardtOptimizer optimizer(*graph, *initialEstimate);
   Values result = optimizer.optimize();
   result.print("Final Result:\n");
 
-  
-  return 0;
+  saveResults(result, outfilename);
+
+//   std::cout << "First pose" << std::endl;
+//   Pose2 pose = result.at(0).cast<Pose2>();
+//   std::cout << pose.x() << " " << pose.y() << " " << pose.theta() << std::endl;
+
+      return 0;
 } 
